283d093e86c4fe188df5a5cfe2ef1e6c29303332
戴海洋 <1037306928@qq.com>
2021-08-12 15:59:33 +0800

    rollup实时刷新

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 import { terser } from "rollup-plugin-terser";
 import serve from "rollup-plugin-serve";
 import clear from "rollup-plugin-clear";
-import { rollup } from "rollup";
-import scss from "rollup-plugin-scss";
+
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import json from "rollup-plugin-json";
+import livereload from "rollup-plugin-livereload";
+import scss from "rollup-plugin-scss";
+import postcss from "rollup-plugin-postcss";
+
+const mode = process.env.NODE_ENV;
+const dev = mode === "dev";
+const build = mode === "build";
 
 export default {
   // 输入
@@ -21,34 +27,38 @@ export default {
     sourcemap: true,
   },
   plugins: [
-    //合并第三方库,告诉 Rollup 如何查找外部模块
-    resolve(),
-    //解决使用三方库，模块导入导出方式
-    commonjs(),
-    //解决es6以上代码兼容性问题的babel
-    babel(),
-    //处理json
-    json(),
     // 代码压缩，去除注释
     terser(),
-    serve({
-      open: true, // 运行时自动打开浏览器
-      headers: {
-        "Access-Control-Allow-Origin": "*", // 本地服务允许跨域
-      },
-      contentBase: ["public", "."], // 本地服务的运行文件根目录
-      port: 5000, // 设置网络服务监听端口
-    }),
+    dev &&
+      serve({
+        open: true, // 运行时自动打开浏览器
+        headers: {
+          "Access-Control-Allow-Origin": "*", // 本地服务允许跨域
+        },
+        contentBase: ["public", "."], // 本地服务的运行文件根目录
+        port: 5000, // 设置网络服务监听端口
+      }),
+    dev && livereload("dist"), //实时刷新
     // 清除目录
     clear({
       targets: ["dist"], // 项目打包编译生成的目录
       watch: true, // 实时监听文件变化
     }),
     // 预处理器
-    rollup({
-      entry: "main.js",
-      plugins: [scss()],
+    scss(),
+    // 支持css文件的加载、css加前缀、css压缩、对scss/less的支持等等
+    postcss(),
+
+    //合并第三方库,告诉 Rollup 如何查找外部模块
+    resolve(),
+    //解决使用三方库，模块导入导出方式
+    commonjs(),
+    //解决es6以上代码兼容性问题的babel
+    babel({
+      babelHelpers: "bundled",
     }),
+    //处理json
+    json(),
   ],
   external: [""], //作用：指出应将哪些模块视为外部模块，否则会被打包进最终的代码里
 };
